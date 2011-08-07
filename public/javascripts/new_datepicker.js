0fabd83133cc7584bf7f2e354c9a267d62187f32
cming <cming.xu@photomugs.com>
2011-08-07 14:44:38 +0800

    datepicker changed

--- public/javascripts/new_datepicker.js
+++ public/javascripts/new_datepicker.js
@@ -2,7 +2,7 @@
   $.fn.new_datepicker = function (options) {
     return this.each(function () {
       $(this).click(function(){
-        WdatePicker({eCont:$(this).get(0),onpicked:function(dp){}, lang:"zh-cn",isShowToday:true})
+        WdatePicker({onpicked:function(dp){}, lang:"zh-cn",isShowToday:true})
       });
     });
   };
