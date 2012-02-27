63274a88ded5e1324f0484db0b9c669a429e164c
kevin.xu <kevin.xu@aurorafeint.com>
2012-02-27 12:53:04 +0800

    remove blance_item old code

--- db/migrate/20110821040439_add_count_amount_to_balance_item.rb
+++
@@ -1,9 +0,0 @@
-class AddCountAmountToBalanceItem < ActiveRecord::Migration
-  def self.up
-    add_column :balance_items, :count_amount, :integer
-  end
-
-  def self.down
-    remove_column :balance_items, :count_amount
-  end
-end
