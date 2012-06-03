d4689ebe4420e28ac98e87e84ed648ff5cec8043
cming <cming.xu@photomugs.com>
2012-06-03 11:28:13 +0800

    abc

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -10,7 +10,7 @@ class CoachesController < ApplicationController
   end
 
   def search
-    @coaches = Coach.paginate(default_paginate_options.merge(:conditions => "lower(pinyin_name) like '%#{params[:q]}%'"))
+    @coaches = Coach.paginate(default_paginate_options.merge(:conditions => "lower(pinyin_name) like '%#{params[:q]}%' or name like '%#{params[:q]}%' "))
     respond_to do |wants|
       wants.json { render :json => @coaches.collect{|c| {"name" => c.name, "id" => c.id, "pinyin_name" => c.pinyin_name}}.to_json}
     end
