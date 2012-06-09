a1576490fdee4d0ee9d8e81f8caca828b3624591
cming <cming.xu@photomugs.com>
2012-06-09 12:51:20 +0800

    abc

--- app/models/order.rb
+++ app/models/order.rb
@@ -205,7 +205,7 @@ class Order < ActiveRecord::Base
     end
 
     def can_cancel?
-       Setting.can_cancel_time_before_activate.from_now < court_book_record.start_time && (not balanced? && !activated?)
+       Setting.can_cancel_time_before_activate.from_now < court_book_record.start_time && (!balanced? && !activated?)
     end
 
     def can_want_sell?
@@ -221,7 +221,7 @@ class Order < ActiveRecord::Base
     end
 
     def can_cancel_all?
-       is_advance_order? && Setting.can_cancel_time_before_activate.from_now < court_book_record.start_time && (not balanced?)
+       is_advance_order? && Setting.can_cancel_time_before_activate.from_now < court_book_record.start_time && (not balanced? && !activated?)
     end
 
     def can_update_all?
