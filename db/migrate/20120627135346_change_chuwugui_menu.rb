c01ed264ebcedd683229254c0505b8e2aea861fe
cming <cming.xu@photomugs.com>
2013-01-26 18:15:07 +0800

    remove unpassed migration

--- db/migrate/20120627135346_change_chuwugui_menu.rb
+++ db/migrate/20120627135346_change_chuwugui_menu.rb
@@ -2,8 +2,8 @@
 
 class ChangeChuwuguiMenu < ActiveRecord::Migration
   def up
-    Power.find(47).update_column(:subject, "储物柜")
-    Power.find(49).update_column(:subject, "储物柜列表")
+    # Power.find(47).update_column(:subject, "储物柜")
+    # Power.find(49).update_column(:subject, "储物柜列表")
   end
 
   def down
