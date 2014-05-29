284f8882b229b30ee98f996a90d5dec0aea6b220
Kevin.Xu <cmxu@kabam.com>
2014-05-29 10:37:11 +0800

    finacial date

--- lib/custom_date_for_report.rb
+++ lib/custom_date_for_report.rb
@@ -1,15 +1,23 @@
 class CustomDateForReport
-  def initialize(date, accounting_begin_day = 1)
+  def initialize(date, accounting_begin_day = 0)
     @date = date
     @accounting_begin_day = accounting_begin_day
   end
 
   def beginning_of_month
-    ((@date - 1.month).beginning_of_month + @accounting_begin_day.days)
+    if @accounting_begin_day > 14
+      ((@date - 1.month).beginning_of_month + @accounting_begin_day.days)
+    else
+      (@date.beginning_of_month + @accounting_begin_day.days)
+    end
   end
 
   def end_of_month
-    (@date.beginning_of_month + (@accounting_begin_day - 1).days)
+    if @accounting_begin_day > 14
+      (@date.beginning_of_month + (@accounting_begin_day - 1).days)
+    else
+      ((@date + 1.month) + (@accounting_begin_day - 1).days)
+    end
   end
 
   def each_day_of_this_financial_month(&block)
